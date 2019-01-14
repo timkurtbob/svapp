753dc72f760241a747c8b4d298a4916bea283d80
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-01-14 13:51:39 +0100

    added all columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_122553) do
+ActiveRecord::Schema.define(version: 2019_01_14_124345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,16 @@ ActiveRecord::Schema.define(version: 2019_01_14_122553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "second_name"
+    t.date "birthday"
+    t.string "class"
+    t.integer "form"
+    t.string "phone"
+    t.string "role"
+    t.string "snapchat_user"
+    t.string "insta_user"
+    t.string "tiktok_user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
