053765542424a3a10dc4cd1ac8678a77824a3481
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-15 17:23:08 +0100

    add bee link

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,19 @@ class EntriesController < ApplicationController
     @author = @entry.user
     @comments = @entry.comments
   end
+
+  def bee
+    @comment = Comment.find(params[:id])
+    @entry = @comment.entry
+    @bee = current_user.bees.find_by(comment_id: @comment.id)
+    if @bee
+      @bee.destroy
+    else
+      Bee.create(comment_id: @comment.id, user_id: current_user.id)
+    end
+    respond_to do |format|
+      format.html { redirect_to entry_path(@entry) }
+      format.js # <-- will render `app/views/entries/bee.js.erb`
+    end
+  end
 end
