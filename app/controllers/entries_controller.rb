29a1466a7a43f35bb2a645ee87a2fac1f1170b37
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-16 15:29:08 +0100

    Create add comment field

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,6 +9,7 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params[:id])
     @author = @entry.user
     @comments = @entry.comments
+    @comment = Comment.new
   end
 
   def bee
@@ -24,5 +25,22 @@ class EntriesController < ApplicationController
       format.html { redirect_to entry_path(@entry) }
       format.js # <-- will render `app/views/entries/bee.js.erb`
     end
+  end
+
+  def add_comment
+    @entry = Entry.find(params[:id])
+    @comment = Comment.new(comment_params)
+    @comment.entry = @entry
+    @comment.user = current_user
+    # authorize @comment
+    if @comment.save
+      redirect_to entry_path(@entry)
+    else
+      render 'new'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
