96f22482f74f5cc09ed604dee23ae6b273226016
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-16 16:05:07 +0100

    Configure database for berlin time

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Svapp
     config.load_defaults 5.2
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :en
+    config.time_zone = "Europe/Berlin"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
