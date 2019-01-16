101011304e33889eac13aa5e6a0381666ce21484
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-16 16:21:28 +0100

    Disable submit button if no input

--- app/javascript/comments/addComment.js
+++ app/javascript/comments/addComment.js
@@ -2,6 +2,7 @@ function addEventListenerforNewComment() {
   const inputField = document.getElementById('comment_text');
   inputField.addEventListener('focusin', showButtons);
   inputField.addEventListener('focusout', hideButtonsIfEmpty);
+  inputField.addEventListener('keyup', toggleSubmitDisable)
 
   const cancelBtn = document.getElementById('cancel-btn');
   cancelBtn.addEventListener('click', hideButtons)
@@ -32,6 +33,16 @@ const hideButtons = () => {
   const buttons = document.querySelector('.form-actions');
   inputField.classList.remove('active')
   buttons.classList.add('d-none');
+}
+
+const toggleSubmitDisable = () => {
+  const inputField = document.getElementById('comment_text');
+  const submitBtn = document.getElementById('submit-btn');
+  if (inputField.value === '') {
+    submitBtn.disabled = true
+  } else {
+    submitBtn.disabled = false
+  }
 }
 
 export { addEventListenerforNewComment };
