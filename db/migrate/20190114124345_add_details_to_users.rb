64390c5bbcf2d038a7323c89bed2348829db9a72
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-01-21 17:21:35 +0100

    updated migration file

--- db/migrate/20190114124345_add_details_to_users.rb
+++ db/migrate/20190114124345_add_details_to_users.rb
@@ -2,13 +2,7 @@ class AddDetailsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :second_name, :string
-    add_column :users, :birthday, :date
-    add_column :users, :klass, :string
-    add_column :users, :form, :integer
     add_column :users, :phone, :string
     add_column :users, :role, :string
-    add_column :users, :snapchat_user, :string
-    add_column :users, :insta_user, :string
-    add_column :users, :tiktok_user, :string
   end
 end
