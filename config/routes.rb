0967d44ac032b89642dbb12e504104aa240ee1e7
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-22 11:26:44 +0100

    set up basic routes/views for profile index and show

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   resources :entries, only: [ :index, :show, :create, :update]
 
   get 'comments/:id/bee', to: 'entries#bee', as: 'bee'
+
   post '/entries/:id/comments', to: 'entries#add_comment', as: 'add_comment'
   get 'entries/:id/bookmark', to: 'entries#bookmark', as: 'bookmark'
   post 'entries/:id/deactivate', to: 'entries#deactivate', as: 'deactivate'
 
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
   # get 'bookmarks', to: 'entries#my_bookmarks', as: 'my_bookmarks'
 end
