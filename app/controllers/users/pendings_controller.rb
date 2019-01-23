d1593a9344df680dff5f863d80d482c7df42e60e
Bryan Leighton <bryan.leigh77@gmail.com>
2019-01-23 15:52:02 +0100

    generate pendings namespace controller

---
+++ app/controllers/users/pendings_controller.rb
@@ -0,0 +1,4 @@
+class Users::PendingsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/users/pendings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Users::PendingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
