53052d74fbc62a75aafa657d6b1545200b8927c4
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-02-19 15:00:59 +0100

    admin dashboard link back to app

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,6 +9,8 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "SV-App"
+  config.site_title_link = :root
+
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
