7e2f3d8345aaae2ed174709142e7eae8f33e2ac1
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-02-19 10:30:54 +0100

    prevent precompiling

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Svapp
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :de
     config.time_zone = "Europe/Berlin"
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
