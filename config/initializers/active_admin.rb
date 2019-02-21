b0fb720dfe43bd1b0d124dcbe4422b556337818d
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-02-21 15:35:39 +0100

    name update

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -8,7 +8,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "SV-App"
+  config.site_title = "SV-App Super-Admin Dashboard"
   config.site_title_link = :root
 
 
