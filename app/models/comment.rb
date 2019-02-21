32470019b3fdbfb31c5eb3d6d7b32e5c4a006089
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-02-21 15:00:16 +0100

    added destroy all connected instances on sadmin delete feature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :entry
-  has_many :bees
+  has_many :bees, dependent: :destroy
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ApplicationRecord
   belongs_to :user
-  has_many :attachments
-  has_many :comments
-  has_many :bookmarks
+  has_many :attachments, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 end
