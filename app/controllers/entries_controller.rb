1bb827705455844f13c736b9e17aee19ac183014
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-02-28 13:00:32 +0100

    remove attach-method from entries_controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -76,15 +76,6 @@ class EntriesController < ApplicationController
     end
   end
 
-  # def add_attachment
-  #   @entry = Entry.find(params[:id])
-  #   authorize @entry
-  #   @attachment = Attachment.new(attachment_params)
-  #   @attachment.entry = @entry
-  #   @attachment.user = current_user
-  #   @attachment.save
-  # end
-
   def add_comment
     @entry = Entry.find(params[:id])
     authorize @entry
@@ -117,10 +108,6 @@ class EntriesController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
-
-  # def attachment_params
-  #   params.require(:attachment).permit(:name)
-  # end
 
   def entry_params
     params.require(:entry).permit(:title, :description, :time, :date, :location)
