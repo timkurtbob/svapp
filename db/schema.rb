e76507a6406cb07c49767a69109448dbc2b8ea08
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-03-04 16:56:27 +0100

    js half way

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_172933) do
+ActiveRecord::Schema.define(version: 2019_03_04_150143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_01_23_172933) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "file_name"
+    t.string "file_type"
     t.index ["entry_id"], name: "index_attachments_on_entry_id"
     t.index ["user_id"], name: "index_attachments_on_user_id"
   end
