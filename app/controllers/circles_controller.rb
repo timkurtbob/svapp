29544d6624d2e3929436512ffeca814b9be56a4d
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-03-09 14:59:22 +0100

    implemented add circle feature

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,8 +1,21 @@
 class CirclesController < ApplicationController
-  after_action :verify_policy_scoped, except: [ :index ]
+  after_action :verify_policy_scoped, except: [ :index, :create ]
 
   def index
     @circles = Circle.all
     authorize @circles
+    @circle = Circle.new
+  end
+
+  def create
+    @circle = Circle.new(circle_params)
+    authorize @circle
+
+    @circle.save
+    redirect_to circles_path
+  end
+
+  def circle_params
+    params.require(:circle).permit(:name)
   end
 end
