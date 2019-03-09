7092933721e3e7f28be7fd5ec59064aab1a41bb3
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-03-09 15:14:30 +0100

    update Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
   belongs_to :user
+  belongs_to :circle
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
