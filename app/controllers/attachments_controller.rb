787d98092170352645af1249efaf3bee11f99148
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-03-12 21:29:25 +0100

    adds attachments render, but does not replace old one + does not delete images from tmp

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,4 +1,5 @@
 class AttachmentsController < ApplicationController
+  after_action :verify_authorized, except: [:create]
   def create
     @entry = Entry.find(params[:entry_id])
     authorize @entry
@@ -8,11 +9,28 @@ class AttachmentsController < ApplicationController
     @attachment.user = @user
     @attachment.entry = @entry
     @attachment.file_name = Time.now.to_s[0..-6]
+    @attachments = attachments_from_same_entry
 
-    @attachment.save
+    if @attachment.save
+      respond_to do |format|
+        format.html { redirect_to @entry }
+        format.js
+      end
+    end
   end
+
+  private
 
   def attachment_params
     params.require(:attachment).permit(:name, :file_type)
+  end
+
+  def attachments_from_same_entry
+    all_attachments = Attachment.all
+    a = []
+    all_attachments.select { |aa| aa.entry == @entry}.each do |x|
+      a << x
+    end
+    return a
   end
 end
