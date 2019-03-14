f8491432d59c0c03ec603df4c1c09aeca9329999
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2019-03-14 11:36:07 +0100

    display filename only for doc attachments

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,8 +2,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   def public_id
-    ts = Time.now.to_s[0..-7]
+    ts = Time.now.to_s[0..-7].gsub(/\s+/, "_-_")
 
-    return "attachments/#{original_filename}_#{ts}"
+    return "attachments/#{original_filename}_--_#{ts}"
   end
 end
